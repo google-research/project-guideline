40bacfa97f6d7242a98a92fdbf2d85286a4eb515
Project Guideline Team <project-guideline-copybara@google.com>
2024-03-05 18:59:42 -0800

    Update tutorial activity user interface
    PiperOrigin-RevId: 613042912

--- project_guideline/environment/path_planning.cc
+++ project_guideline/environment/path_planning.cc
@@ -280,6 +280,21 @@ RotationMovementOutput SimpleControlSystem::ComputeRotationalMovement(
   return RotationMovementOutput(rotation_movement_degrees, all_rotations,
                                 num_guideline_points_per_segment);
 }
+RotationMovementOutput
+SimpleControlSystem::ComputeRotationalMovementWithLookAhead(
+    const Vector3d& human_direction,
+    absl::Span<const Vector3d> guideline_points,
+    int closest_guideline_point_indx, float rotational_movement_ahead_meter,
+    const Axis3 vertical_axis) {
+  auto indices_skip =
+      static_cast<size_t>(rotational_movement_ahead_meter *
+                          options_.num_guideline_points_per_meter());
+  auto closest_guideline_point_indx_offset =
+      closest_guideline_point_indx + indices_skip;
+  return ComputeRotationalMovement(human_direction, guideline_points,
+                                   closest_guideline_point_indx_offset,
+                                   vertical_axis);
+}
 
 TurnPointOutput SimpleControlSystem::FindTurnPoint(
     const Vector3d& human_position, absl::Span<const Vector3d> guideline_points,
@@ -314,14 +329,14 @@ TurnPointOutput SimpleControlSystem::FindTurnPoint(
   return TurnPointOutput(turn_point, turn_angle, turn_point_distance_meters);
 }
 
-bool SimpleControlSystem::IsStopCondition() {
+int SimpleControlSystem::IsStopCondition() {
   {
     absl::MutexLock lock(&control_signal_history_lock_);
 
     auto current_control_signal = control_signal_history_.back();
     // Out of guideline scenario.
     if (std::isnan(current_control_signal.lateral_movement_meters)) {
-      return true;
+      return 4;
     }
 
     // Deviation from the guideline scenario.
@@ -351,19 +366,21 @@ bool SimpleControlSystem::IsStopCondition() {
         }
       }
       if (!has_in_range_movement) {
-        return true;
+        return 5;
       }
     }
-    return false;
+    return 0;
   }
 }
 
 // Post processes and returns final control signal to be communicated to the
 // human.
 const ControlSignal SimpleControlSystem::PostProcessControlSignals() {
-  if (IsStopCondition()) {
+  int stop_condition = IsStopCondition();
+  if (stop_condition != 0) {
     ControlSignal stop_signal;
     stop_signal.stop = true;
+    stop_signal.stop_reason = stop_condition;
     obstacle_latch_.Reset();
     return stop_signal;
   }
@@ -447,6 +464,10 @@ ControlSignal SimpleControlSystem::GenerateControlSignal(
     auto rotational_movement = ComputeRotationalMovement(
         human_direction, guideline_points,
         lateral_movement.closest_guideline_point_indx, vertical_axis);
+    auto rotational_movement_ahead = ComputeRotationalMovementWithLookAhead(
+        human_direction, guideline_points,
+        lateral_movement.closest_guideline_point_indx,
+        options_.rotational_movement_ahead_meters(), vertical_axis);
     auto turn_point = FindTurnPoint(
         human_position, guideline_points,
         lateral_movement.closest_guideline_point_indx,
@@ -458,6 +479,8 @@ ControlSignal SimpleControlSystem::GenerateControlSignal(
         lateral_movement.lateral_movement_meters;
     current_control_signal.rotation_movement_degrees =
         rotational_movement.rotation_movement_degrees;
+    current_control_signal.rotation_movement_ahead_degrees =
+        rotational_movement_ahead.rotation_movement_degrees;
     current_control_signal.turn_point = turn_point.turn_point;
     current_control_signal.turn_angle_degrees = turn_point.turn_angle_degrees;
     current_control_signal.turn_point_distance_meters =
